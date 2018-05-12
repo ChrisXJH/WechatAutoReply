2276f4cf4e9c9bdbf465c70dd623fec5ab04e341
ChrisXJH <j352xu@edu.uwaterloo.ca>
2018-05-11 20:17:58 -0400

    Keep session active by sending empty messages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const wchat4u = require('wechat4u');
 const qrcode = require('qrcode-terminal');
-const fs = require('fs');
 let Wechat = new wchat4u();
 const Robot = require('./robot')(Wechat);
 
 Wechat.start();
+
+// Keep wechat session active
+function keepAlive() {
+    setInterval(() => {
+        Wechat.sendMsg('', 'filehelper')
+        .catch(err => {
+            console.error(err);
+        });
+    }, 5 * 60 * 1000);
+}
 
 Wechat.on('uuid', uuid => {
     qrcode.generate('https://login.weixin.qq.com/l/' + uuid, {
@@ -17,6 +26,7 @@ Wechat.on('uuid', uuid => {
 Wechat.on('login', () => {
     console.log('Logged in successfully!');
     Robot.start();
+    keepAlive();
 });
 
 Wechat.on('logout', () => {
